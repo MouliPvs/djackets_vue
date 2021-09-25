9e15ebed3e02da034e94bb051097708e6faaeb91
MouliPvs <mouli.pvs@gmail.com>
2021-09-25 10:06:28 +0530

    Added Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+    <!-- Navigation Bar With Dark Theme Start-->
+    <nav class="navbar is-dark">
+      
+      <!-- Logo -->
+      <div class="navbar-brand">
+        <!-- Nav Item Djacket -->
+        <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
+
+        <!-- Hamburger Button -->
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <!-- Logo Ends -->
+
+      <!-- Navigation Bar Menu Start-->
+      <div class="navbar-menu" id="navbar-menu">
+
+        <!--Nav Itemts At End / right Side -->
+        <div class="navbar-end">
+          <!-- Nav Item : Summer  -->
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+
+          <!-- Nav Item : Winter  -->
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+
+          <!-- Nav Items : Buttons Start -->
+          <div class="navbar-item">
+            <div class="buttons">
+              <!--Nav Item :  Login Start -->
+              <router-link to="/log-in" class="button is-light">Log In</router-link>
+              
+              <!--Nav Item :  Cart  -->
+              <router-link to="/cart" class="button is-success">
+
+              <!-- Cart Icon -->
+              <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+              <span>Cart</span>
+              </router-link>  
+              <!--Nav Item :  Login End -->
+            </div>
+          </div>
+          <!-- Nav Items : Buttons End -->
+        </div>        
+      </div>
+      <!-- Navigation Bar Menu End-->
+    </nav>
+    <!-- Navigation Bar With Dark Theme Ends-->
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <!-- Fotter -->
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
+
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma'
 </style>
