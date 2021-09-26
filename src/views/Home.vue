9a4032dc41cb16969e5b67aec18a424df4e27d28
MouliPvs <mouli.pvs@gmail.com>
2021-09-26 16:33:11 +0530

    Added Fetching Data From backend Functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
 
      <!-- hero component allows you to add a full width banner to your webpage -->
      <!-- hero as the main container -->
+     <!-- Welcome To Djackets -->
     <section class="hero is-medium is-dark mb-6">
     
       <!-- hero-body as a direct child, in which you can put all your content -->
@@ -18,11 +19,10 @@
     
       </div> <!-- Hero-Body End -->
     
-    </section> <!-- Hero End -->
+    </section> <!-- Welcome To Djackets End -->
 
     <!-- Latest Products -->
     <div class="columns is-multiline">
-
       
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest Products</h2>
@@ -37,14 +37,14 @@
         <!-- A white box to contain other elements -->
         <div class="box">
 
-          <!-- ThumbNail -->
+          <!-- Product ThumbNail -->
           <figure class="image mb-4">
             <img src="product.get_thumbnail">
           </figure>
 
           <h3 class="is-size-4">{{product.name}}</h3>
           <p class="is-size-6 has-text-gery">{{product.price}} Rs</p>
-          
+
         </div>
       </div> <!-- Product Details End -->
 
@@ -54,11 +54,45 @@
 </template>
 
 <script>
-// @ is an alias to /src
+// For getting data from backend
+import axios from 'axios'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
   components: {
+  },
+
+  // Used for fetching data
+  mounted() {
+    this.getLatestProducts()
+  },
+
+  methods: {
+    getLatestProducts(){
+    // Gets latest products details in the form of array from api/v1/latest-products
+      axios
+          .get('/api/v1/latest-products/')
+          .then(response => {
+            this.latestProducts = response.data
+          })//response
+          .catch(error => {
+          console.log(error)
+          })//error
+    }
   }
 }
 </script>
+
+<style scoped>
+  /* Removes empty spaces around the image  */
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
